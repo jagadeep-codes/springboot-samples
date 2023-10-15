f423abb20f96817044737964608fffa2814ff646
jagadeep-codes <jagadeep.codes@gmail.com>
2023-10-15 21:09:33 +0530

    Added code to log total time for method execution

--- perf-monitor/src/main/java/com/practice/monitor/MonitorApplication.java
+++ perf-monitor/src/main/java/com/practice/monitor/MonitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValidatorApplication {
+public class MonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidatorApplication.class, args);
+		SpringApplication.run(MonitorApplication.class, args);
 	}
 
 }
